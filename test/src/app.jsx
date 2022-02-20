64ec7e3c45f0c5012af11f1c121921807c85210b
purplesheep513 <prune1231@gmail.com>
2022-02-20 22:04:28 +0900

    간단한 코드작성

--- test/src/app.jsx
+++ test/src/app.jsx
@@ -1,8 +1,18 @@
-import './App.css';
-
+import React from 'react';
+import './app.css';
+//<React.Fragment> 는 다수의 태그를 리턴할 때 사용한다.(<></>도 가능) 예전엔 div를 대신 사용했지만 굳이 감싸줄 필요가 없다면 <React.Fragment>를 쓴다.
 function App() {
+  const name = 'bora'
   return (
-    <h1>hello :)</h1>
+    <>
+      <h1>welcome</h1>
+      {
+        name && <h1>Hello {name} :)</h1>
+      }
+      {['❤️', '🍎'].map(item => (
+        <h1>{item}</h1>
+      ))}
+    </>
   );
 }
 
